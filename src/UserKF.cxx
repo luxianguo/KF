0ee44995fbc23cb4189e65ea98a6fa49697cdd1e
Kang Yang <kang.yang.oxford@gmail.com>
2021-09-29 15:58:49 -0500

    Changed variable names and make the toy example structure more like protoDUNE

--- src/UserKF.cxx
+++ src/UserKF.cxx
@@ -32,18 +32,18 @@ public:
     }
     
     fLambda = par[0];
-    fX      = par[1];
-    fY      = par[2];
-    fZ      = par[3];
+    fOptX   = par[1];
+    fOptY   = par[2];
+    fOptZ   = par[3];
 
   }
 
   static double Constraint(){
-    return fX + fY + fZ - 29;
+    return fOptX + fOptY + fOptZ - 29;
   }
 
   static double FullLikelihood(){
-    return CoreLikelihood(E1,E2,E3) + fLambda * Constraint();
+    return CoreLikelihood() + fLambda * Constraint();
   }
   
   static void IniCoreMIN(TMinuit * mnt, const double inputl){
@@ -51,9 +51,9 @@ public:
     mnt->FixParameter(0);  // Fix Lambda
     
     //user previous fit result as initial values
-    mnt->DefineParameter(1, "x", fX, 1e-2, -1e6, 1e6);
-    mnt->DefineParameter(2, "y", fY, 1e-2, -1e6, 1e6);
-    mnt->DefineParameter(3, "z", fZ, 1e-2, -1e6, 1e6);
+    mnt->DefineParameter(1, "x", fOptX, 1e-2, -1e6, 1e6);
+    mnt->DefineParameter(2, "y", fOptY, 1e-2, -1e6, 1e6);
+    mnt->DefineParameter(3, "z", fOptZ, 1e-2, -1e6, 1e6);
   }
 
   static bool IsConstraintGood(){
@@ -61,72 +61,58 @@ public:
     return (TMath::Abs(Constraint())<eps);
   }
 
-  static TMatrixD CovarianceMatrix(){
-    // Defines the covariance matrix and the variables
-    double V[9];
-    // Set the covariance matrix elements 
-    for(int i=0;i<3;i++) {
-      for(int j=0;j<3;j++) {
-        V[3*i+j]= 10E-5; //off diagonal elements have no co-correlation here but a small number here does no effect the algorithm
-        if(i==j && (i==0)) {V[3*i+j]=TMath::Power(0.5,2.);}
-        if(i==j && (i==1)) {V[3*i+j]=TMath::Power(1,2.);}
-        if(i==j && (i==2)) {V[3*i+j]=TMath::Power(1,2.);}
-      }
-    }
-    // Create the matrix
-    int nparameters = fNpar-1;
-    TMatrixD CovMatrix(nparameters,nparameters);
-    for(int i=0;i<nparameters*nparameters;i++) {
-      int x=floor(i/nparameters);
-      int y=i%nparameters;
+  static void Print(const TString tag){
+    printf("%20s lambda %10.6e X %10.6e Y %10.6e, core %20.6e constraint %20.6e full %20.6e\n", tag.Data(), fLambda, fOptX, fOptY, CoreLikelihood(), Constraint(), FullLikelihood());
+  }
+  static double GetfOptX(){return fOptX;}
+  
+  static double GetfOptY(){return fOptY;}
+
+  static double GetfOptZ(){return fOptZ;}
+  
+  static double GetfLambda(){return fLambda;}
+  
+  static double GetfNpar(){return fNpar;}
+  
+  static void SetVars(double iniVarX, double iniVarY, double iniVarZ){
+    fX = iniVarX;
+    fY = iniVarY;
+    fZ = iniVarZ;
+  }
+  static void SetCVM(vector<double> V){
+    int size = V.size();
+    int dim = sqrt(size);
+    for(int i=0;i<size;i++) {
+      int x=floor(i/dim);
+      int y=i%dim;
       CovMatrix[x][y]=V[i];
     }
-    return CovMatrix;
-  }
-
-  static void Print(const TString tag){
-    printf("%20s lambda %10.6e X %10.6e Y %10.6e, core %20.6e constraint %20.6e full %20.6e\n", tag.Data(), fLambda, fX, fY, CoreLikelihood(E1,E2,E3), Constraint(), FullLikelihood());
-  }
-  static double GetfX(){
-    return fX;
-  }
-  static double GetfY(){
-    return fY;
-  }
-  static double GetfZ(){
-    return fZ;
-  }
-  static double GetfLambda(){
-    return fLambda;
-  }
-  static void Set(double e1, double e2, double e3){
-    E1 = e1;
-    E2 = e2;
-    E3 = e3;
   }
 private:
   static double fLambda;
+  static double fOptX;
+  static double fOptY;
+  static double fOptZ;
+  static const int fNpar;
+
   static double fX;
   static double fY;
   static double fZ;
-  static const int fNpar;
 
-  static double E1;
-  static double E2;
-  static double E3;
+  static TMatrixD CovMatrix;
 
-  
-  static double CoreLikelihood(double E1, double E2, double E3){
+  static double CoreLikelihood(){
 
-    TMatrixD CovMatrix = CovarianceMatrix();
-    TMatrixD CovMatrixInverse = CovMatrix.Invert();
+    TMatrixD CovMatrixtmp = CovMatrix;
+    // Note Invert() will change the original matrix, need to use a copy
+    TMatrixD CovMatrixInverse = CovMatrixtmp.Invert();
 
     int nparameters = fNpar-1;
 
     TMatrixD Diff(nparameters,1);
-    Diff[0][0]=fX-E1;
-    Diff[1][0]=fY-E2;
-    Diff[2][0]=fZ-E3;
+    Diff[0][0]=fOptX-fX;
+    Diff[1][0]=fOptY-fY;
+    Diff[2][0]=fOptZ-fZ;
 
     TMatrixD DiffT(1,nparameters);
     DiffT.Transpose(Diff);
@@ -139,14 +125,18 @@ private:
 };
 
 double UserKF::fLambda = -999;
+double UserKF::fOptX = -999;
+double UserKF::fOptY = -999;
+double UserKF::fOptZ = -999;
+const int UserKF::fNpar = 4;
+
 double UserKF::fX = -999;
 double UserKF::fY = -999;
 double UserKF::fZ = -999;
-const int UserKF::fNpar = 4;
 
-double UserKF::E1 = -999;
-double UserKF::E2 = -999;
-double UserKF::E3 = -999;
+const int npars = UserKF::GetfNpar() - 1; 
+TMatrixD UserKF::CovMatrix(npars,npars);
+
 
 vector<double> IniE(){
 
@@ -158,16 +148,34 @@ vector<double> IniE(){
   std::normal_distribution<double> nd2(9*0.8, 1); 
   std::normal_distribution<double> nd3(16*0.8, 1); 
   // Generate the intial E's value
-  double E1 = nd1(engine);
-  double E2 = nd2(engine);
-  double E3 = nd3(engine);
+  double iniX = nd1(engine);
+  double iniY = nd2(engine);
+  double iniZ = nd3(engine);
   vector<double> vet;
-  vet.push_back(E1);
-  vet.push_back(E2);
-  vet.push_back(E3);
-  UserKF::Set(E1,E2,E3);
+  vet.push_back(iniX);
+  vet.push_back(iniY);
+  vet.push_back(iniZ);
   return vet;
 
+}
+
+vector<double> GetCVM(){
+  // Defines the covariance matrix and the variables
+  double CVM[9];
+  vector<double> CVM_Vet;
+  // Set the covariance matrix elements 
+  for(int i=0;i<3;i++) {
+    for(int j=0;j<3;j++) {
+      CVM[3*i+j]= 10E-5; //off diagonal elements have no co-correlation here but a small number here does no effect the algorithm
+      if(i==j && (i==0)) {CVM[3*i+j]=TMath::Power(0.5,2.);}
+      if(i==j && (i==1)) {CVM[3*i+j]=TMath::Power(1,2.);}
+      if(i==j && (i==2)) {CVM[3*i+j]=TMath::Power(1,2.);}
+    }
+  }
+  for(int i=0;i<9;i++){
+    CVM_Vet.push_back(CVM[i]);
+  }
+  return CVM_Vet;
 }
 
 void CoreFCN(int &npars, double *grad, double &value, double *par, int flag)
@@ -179,8 +187,8 @@ void CoreFCN(int &npars, double *grad, double &value, double *par, int flag)
 
 void LambdaFCN(int &npars, double *grad, double &value, double *par, int flag)
 {
-  //parameters and results are passed in this order
-  //par/UserKF -> CoreMIN -> UserKF -> value
+  // Parameters and results are passed in this order
+  // Par/UserKF -> CoreMIN -> UserKF -> value
   
   //UserKF::Print("\nLambdaFCN before fit");
 
@@ -216,15 +224,25 @@ void LambdaFCN(int &npars, double *grad, double &value, double *par, int flag)
     value = 1E50;//must be large enough wrt possible FullLikelihood when fit fail
   }
 
-  //already set during CoreMIN
-  //UserKF::SetPar(CoreMIN);//only save fit values to MIN when converge
-
   delete CoreMIN;
 }
 
-bool DoubleMin(const double iniLambda, const double lmin, const double lmax)
+void SetIniValues(vector<double> iniVar, vector<double> CVM){
+  int VarsSize = iniVar.size();
+  int CVMDim = sqrt(CVM.size());
+  if(VarsSize != CVMDim) {
+    cout <<  "Variable size is not the same as CVM dimension! " << VarsSize << " " << CVMDim << endl;
+    exit(1);
+  }
+  UserKF::SetVars(iniVar[0],iniVar[1],iniVar[2]);
+  UserKF::SetCVM(CVM);
+}
+
+
+bool DoubleMin(const double iniLambda, const double lmin, const double lmax, vector<double> iniVar, vector<double> CVM, int& GoodFit)
 {
   //UserKF::Print("DoubleMin before fit\n");
+  SetIniValues(iniVar, CVM);
 
   TMinuit * LambdaMIN = new TMinuit(1);
   LambdaMIN->SetPrintLevel(-1);
@@ -237,7 +255,7 @@ bool DoubleMin(const double iniLambda, const double lmin, const double lmax)
   //UserKF::Print("DoubleMin after fit\n");
   
   int irun = 1;
-  const int maxnrun=20;//if time permits, the larger the better  
+  const int maxnrun=20; //if time permits, the larger the better  
   while(flag!=0 || ! UserKF::IsConstraintGood()){
     printf("LambdaMIN bad fit! %d %e ------- run once more! [%d]\n", flag, UserKF::Constraint(), irun++);
     
@@ -253,7 +271,8 @@ bool DoubleMin(const double iniLambda, const double lmin, const double lmax)
 
   if(flag==0 && UserKF::IsConstraintGood()){
     printf("DoubleMin finishes: it works for %f %f %f\n", iniLambda, lmin, lmax);
-    cout << "X: " <<  UserKF::GetfX() << " Y: " <<  UserKF::GetfY() << " Z: " <<  UserKF::GetfZ()<< " lambda: " <<   UserKF::GetfLambda() << endl;
+    //cout << "X: " <<  UserKF::GetfOptX() << " Y: " <<  UserKF::GetfOptY() << " Z: " <<  UserKF::GetfOptZ()<< " lambda: " <<   UserKF::GetfLambda() << endl;
+    GoodFit = 0;
     return true;
   }
   else{
@@ -270,23 +289,28 @@ int main()
 
   for(int i = 0; i < 1000; i++){
     vector<double> vet = IniE();
+    vector<double> CVMvet = GetCVM();
 
     for(unsigned int i = 0; i < vet.size(); i++){
       cout << "vet: " << vet[i] << endl;
     }
-    hBefore->Fill(vet[0]);
-    hBefore->Fill(vet[1]);
-    hBefore->Fill(vet[2]);
+    int GoodFit = -999;
 
-    DoubleMin(5, -10, 100);
+    DoubleMin(5, -10, 100, vet, CVMvet, GoodFit);
 
-    cout << "xxX: " <<  UserKF::GetfX() << " Y: " <<  UserKF::GetfY() << " Z: " <<  UserKF::GetfZ()<< " lambda: " <<   UserKF::GetfLambda() << endl;
-    double E1 = UserKF::GetfX();
-    double E2 = UserKF::GetfY();
-    double E3 = UserKF::GetfZ();
-    hAfter->Fill(E1);
-    hAfter->Fill(E2);
-    hAfter->Fill(E3);
+    if(GoodFit != -999){
+      // Fill histograms for good fit
+      hBefore->Fill(vet[0]);
+      hBefore->Fill(vet[1]);
+      hBefore->Fill(vet[2]);
+      double finX = UserKF::GetfOptX();
+      double finY = UserKF::GetfOptY();
+      double finZ = UserKF::GetfOptZ();
+      hAfter->Fill(finX);
+      hAfter->Fill(finY);
+      hAfter->Fill(finZ);
+    }
+    else cout << "This event is not well fitted, discard!" << endl;
   }
 
   TCanvas * c1 = new TCanvas("c1", "", 1200, 800);
@@ -305,22 +329,6 @@ int main()
   legend->AddEntry(hAfter,"After Fitting","f");
   legend->Draw("same");
   c1->Print("hEnergyFitting.png");
-
-  /* Other tests
-  DoubleMin(0.5, 0, 1000);
-  DoubleMin(  5, 0, 100);
-  DoubleMin( 10, 0, 100);
-  DoubleMin( 10, 0, 1000);//fail -> works now after changing the range and step of X and Y to general ones
-  DoubleMin(  5, -1, 100);
-  DoubleMin(5, -10, 100);//fail at maxnrun =4, but works with maxnrun >= 9
-  DoubleMin(5, 0, 100);
-  DoubleMin(5, -100, 100);//works 3 tries
-  DoubleMin(10, -1e6, 1e6);//fail eventually: lambda can't be too large
-  DoubleMin(10, -1e3, 1e3);
-  DoubleMin(0, -1e3, 1e3);//fail: lambda can't start as 0
-  DoubleMin(1e-2, -1e3, 1e3);//
-  */
-
 
   return 0;
 }
